af52bac45ef1c4a017f02d79f9899fb1716400d4
pezzaliapp <pezzalialessandro@gmail.com>
2026-01-06 07:50:16 +0100

    Implement service worker for caching assets

---
+++ sw.js
@@ -0,0 +1,24 @@
+const CACHE = 'releasepilot-v1';
+const ASSETS = ['./', './index.html', './sw.js'];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE).then((cache) => cache.addAll(ASSETS))
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((keys) =>
+      Promise.all(keys.map((k) => (k !== CACHE ? caches.delete(k) : null)))
+    )
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((cached) => cached || fetch(event.request).catch(() => caches.match('./index.html')))
+  );
+});
